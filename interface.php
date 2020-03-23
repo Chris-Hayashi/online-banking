34e0789b2df29f8c18caea1ac474d5277c16de4c
Christopher Hayashi <Chrisrhayashi@gmail.com>
2020-03-23 14:57:03 -0700

    uploading php files

---
+++ interface.php
@@ -0,0 +1,45 @@
+<html>
+    <head>
+        <!-- Import Bootstrap -->
+    </head>
+
+    <body>
+        <!-- Display First Name and Last Name -->
+
+        <!-- Display User ID -->
+
+        <!-- Display Account Balance -->
+
+        <!-- START MENU -->
+
+            <!-- Deposit Button -->
+
+            <!-- Withdraw Button -->
+
+            <!-- Transfer Button -->
+
+            <!-- Close Account Button -->
+
+            <!-- Logout Button -->
+
+        <!-- END MENU -->
+        
+        <!-- DEPOSIT MODAL -->
+
+            <!-- START FORM -->
+
+                <!-- Enter deposit amount -->
+
+                <!-- Deposit button (submit) -->
+
+            <!-- END FORM -->
+
+        <!-- END MODAL -->
+
+        <!-- WITHDRAW MODAL -->
+
+            
+        <!-- END MODAL -->
+
+    </body>
+</html>
---
+++ landing.php
@@ -0,0 +1,19 @@
+<html>
+    <head>
+    <!-- Import Bootstrap -->
+    </head>
+
+    <body>
+
+    <!-- WELCOME -->
+
+    <!-- LANDING PAGE MENU -->
+
+        <!-- Create an Account Button (redirects to register.php) -->
+
+        <!-- Login Button (redirects to login.php) -->
+
+    <!-- END MENU -->
+
+    </body>
+</html>
